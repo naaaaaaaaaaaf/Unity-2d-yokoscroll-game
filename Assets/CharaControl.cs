30fbf518cb0db24f9e752edb5547645f387a5ef2
naf <naf@auri.ga>
2020-09-28 20:53:45 +0900

    ç§»åå¦çãè¿½å 

---
+++ Assets/CharaControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaControl : MonoBehaviour
+{
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        Vector2 movement = new Vector2(x, 0);
+        rigidbody.AddForce(movement * speed);
+    }
+}
